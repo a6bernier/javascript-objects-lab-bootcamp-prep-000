5e311170c3af113a7f7564d045b5c96897212b18
Andrew Bernier <a6bernier@gmail.com>
2018-07-31 02:02:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = ["value" , "key"]
+  object.key = "value"
   return object
 }
