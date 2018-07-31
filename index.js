6a7a1a6ff724cb0b901dbe19b5798bd2f8732840
Andrew Bernier <a6bernier@gmail.com>
2018-07-31 02:04:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function  deleteFromObjectByKey(object, key)
+function deleteFromObjectByKey(object, key)
